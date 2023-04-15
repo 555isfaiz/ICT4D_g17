e5a0abc2f5c38b0cd82df0da2843bf5c4245affe
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2023-04-15 13:34:18 +0200

    weather: added initial script

--- weather.py
+++ weather.py
@@ -1,1 +1,14 @@
 # Call Openweathermap API according to args
+import requests
+
+api_key = '9548a6803dc1bb21607ca8df16793289'  # replace with your actual API key
+city_name = 'Tingoli'  # replace with the name of the city you want to retrieve weather data for
+api_url = f'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={api_key}'
+
+response = requests.get(api_url)
+if response.status_code == 200:
+    data = response.json()
+    # print(data)
+    # do something with the weather data
+else:
+    print('Error:', response.status_code)
